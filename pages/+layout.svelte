6badc5859f8e60b995eec19dd316ae285d5c2964
Youssef-Harby <youssef_harby@yahoo.com>
2025-03-30 05:53:49 +0200

    branding

---
+++ pages/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+  export let data;
+</script>
+
+<EvidenceDefaultLayout 
+  {data} 
+  title="Cloud Native Weather Station Dashboard"
+  builtWithEvidence={false}
+  hideBreadcrumbs={true}
+  githubRepo="https://github.com/Youssef-Harby"
+  xProfile="https://x.com/youssef_7arby"
+>
+  <slot slot="content" />
+</EvidenceDefaultLayout>
